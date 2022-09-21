2c2b74d42b4b4d1b49aa8c5ce30149eaf44aa8ec
DevaWibowo <50189150+DevaWibowo@users.noreply.github.com>
2022-09-21 16:46:27 +0700

    Add files via upload

---
+++ SEA2/src/main/java/com/seania/sea2/Karakter.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.seania.sea2;
+
+/**
+ *
+ * @author kuma
+ */
+public class Karakter {
+    public Karakter chara;
+    private String Nama;
+
+    /**
+     * @return the Nama
+     */
+    public String getNama() {
+        return Nama;
+    }
+
+    /**
+     * @param Nama the Nama to set
+     */
+    public void setNama(String Nama) {
+        this.Nama = Nama;
+    }
+}
---
+++ SEA2/src/main/java/percobaan/Chara.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package percobaan;
+
+/**
+ *
+ * @author kuma
+ */
+public class Chara {
+    private String Nama;
+
+    /**
+     * @return the Nama
+     */
+    public String getNama() {
+        return Nama;
+    }
+
+    /**
+     * @param Nama the Nama to set
+     */
+    public void setNama(String Nama) {
+        this.Nama = Nama;
+    }
+}
